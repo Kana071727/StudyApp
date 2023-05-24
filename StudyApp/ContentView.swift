6a98df7c639eb58359b846b2e473a924f8bf5289
Kana071727 <zushimikana0213@gmail.com>
2023-05-24 19:48:27 +0900

    Initial Commit

---
+++ StudyApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudyApp
+//
+//  Created by 清水　佳奈音 on 2023/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyApp/StudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAppApp.swift
+//  StudyApp
+//
+//  Created by 清水　佳奈音 on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
