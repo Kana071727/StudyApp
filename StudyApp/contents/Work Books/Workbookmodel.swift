1155cf0d36945e8de5cab1e75b544a4e6ef16317
Kana071727 <zushimikana0213@gmail.com>
2023-08-30 16:21:49 +0900

    問題集　保存設定し始め

---
+++ StudyApp/contents/Work Books/Workbookmodel.swift
@@ -0,0 +1,18 @@
+//
+//  Workbookmodel.swift
+//  StudyApp
+//
+//  Created by 清水　佳奈音 on 2023/08/30.
+//
+
+import SwiftUI
+
+struct Workbookmodel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Workbookmodel()
+}
